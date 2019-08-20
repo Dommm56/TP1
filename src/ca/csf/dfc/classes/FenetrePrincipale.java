0932580cb81b98d40d5d9417028ae7e1ba5dfb03
Dom <dom.legendre@hotmail.com>
2019-08-20 16:25:52 -0400

    ajout des enregistrement bugs

--- src/ca/csf/dfc/classes/FenetrePrincipale.java
+++ src/ca/csf/dfc/classes/FenetrePrincipale.java
@@ -14,20 +14,25 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
-import com.sun.glass.ui.Application;
-
 import javax.swing.JFileChooser;
 import java.io.File;
-import java.io.FileWriter; 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
+import org.xml.sax.*;
+import org.w3c.dom.*;
 
 public class FenetrePrincipale extends JFrame {
 	private static final long serialVersionUID = 1L;
-
+	
 	public FenetrePrincipale(JPanel p_PanneauPrincipal,JPanel p_PanneauSecondaire)
     {
-        super("TP POO");
+        super("Projet Dessin");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.definirMenu();
         this.setLayout(new BorderLayout());
@@ -52,7 +57,8 @@ public class FenetrePrincipale extends JFrame {
 		JMenu menuFichier = new JMenu("Fichier");
 		JMenuItem itm_Nouveau = new JMenuItem("Nouveau");
 		JMenuItem itm_Ouvrir = new JMenuItem("Ouvrir");
-		JMenuItem itm_Enregistrer = new JMenuItem("Enregistrer");
+		JMenuItem itm_EnregistrerXML = new JMenuItem("Enregistrer XML");
+		JMenuItem itm_EnregistrerSVG= new JMenuItem("Enregistrer SVG");
 		JMenuItem itm_Quitter = new JMenuItem("Quitter");
 		
 		private void definirMenu() {
@@ -60,18 +66,20 @@ public class FenetrePrincipale extends JFrame {
 			
 			this.menuFichier.add(this.itm_Nouveau);
 			this.menuFichier.add(this.itm_Ouvrir);
-			this.menuFichier.add(this.itm_Enregistrer);
+			this.menuFichier.add(this.itm_EnregistrerXML);
+			this.menuFichier.add(this.itm_EnregistrerSVG);
 			this.menuFichier.add(this.itm_Quitter);
 			
 			//definir OUVRIR
 			this.itm_Ouvrir.addActionListener(new GestOuvrir());
 			this.itm_Quitter.addActionListener(new GestQuitter());
 			this.itm_Nouveau.addActionListener(new GestNouveau());
-			this.itm_Enregistrer.addActionListener(new GestEnregistrer());
+			this.itm_EnregistrerXML.addActionListener(new GestEnregistrerXML());
+			this.itm_EnregistrerSVG.addActionListener(new GestEnregistrerSVG());
 			
 			}
 			
-			
+		
 		}//fin
 		
 		class GestOuvrir implements ActionListener {
@@ -119,9 +127,9 @@ public class FenetrePrincipale extends JFrame {
 							
 		}//fin GestNouveau
 		
-		class GestEnregistrer implements ActionListener {
+		class GestEnregistrerXML implements ActionListener {
 
-			public GestEnregistrer() {
+			public GestEnregistrerXML() {
 				// TODO Auto-generated constructor stub
 			}
 
@@ -130,12 +138,150 @@ public class FenetrePrincipale extends JFrame {
 				JFileChooser fc = new JFileChooser();
 		          fc.setCurrentDirectory( new File( System.getProperty( "user.dir" ) ) );
 		          int rsp = fc.showSaveDialog(fc) ;
-
-				}//fin actionPerformed
+		          String filename = fc.getSelectedFile().getName();
+		          saveToXML(filename);
+				}//fin actionPerformed	
 			
-				
+			public void saveToXML(String xml) {
+				String role1 = null;
+				String role2 = null;
+				String role3 = null;
+				String role4 = null;
+				ArrayList<String> rolev;
+
+			    Document dom;
+			    org.w3c.dom.Element e = null;
+
+			    // instance of a DocumentBuilderFactory
+			    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			    try {
+			        // use factory to get an instance of document builder
+			        DocumentBuilder db = dbf.newDocumentBuilder();
+			        // create instance of DOM
+			        dom = db.newDocument();
+
+			        // create the root element
+			        org.w3c.dom.Element rootEle = dom.createElement("roles");
+
+			        // create data elements and place them under root
+			        e = dom.createElement("role1");
+			        e.appendChild(dom.createTextNode(role1));
+			        rootEle.appendChild(e);
+
+			        e = dom.createElement("role2");
+			        e.appendChild(dom.createTextNode(role2));
+			        rootEle.appendChild(e);
+
+			        e = dom.createElement("role3");
+			        e.appendChild(dom.createTextNode(role3));
+			        rootEle.appendChild(e);
+
+			        e = dom.createElement("role4");
+			        e.appendChild(dom.createTextNode(role4));
+			        rootEle.appendChild(e);
+
+			        dom.appendChild(rootEle);
+
+			        try {
+			            Transformer tr = TransformerFactory.newInstance().newTransformer();
+			            tr.setOutputProperty(OutputKeys.INDENT, "yes");
+			            tr.setOutputProperty(OutputKeys.METHOD, "xml");
+			            tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+			            tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
+			            tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+			            // send DOM to file
+			            tr.transform(new DOMSource(dom), 
+			                                 new StreamResult(new FileOutputStream(xml)));
+
+			        } catch (TransformerException te) {
+			            System.out.println(te.getMessage());
+			        } catch (IOException ioe) {
+			            System.out.println(ioe.getMessage());
+			        }
+			    } catch (ParserConfigurationException pce) {
+			        System.out.println("UsersXML: Error trying to instantiate DocumentBuilder " + pce);
+			    }
+			}//fin methode 
 		}//fin GestEnregistrer
 		
+		class GestEnregistrerSVG implements ActionListener {
+
+			public GestEnregistrerSVG() {
+				// TODO Auto-generated constructor stub
+			}
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fc = new JFileChooser();
+		          fc.setCurrentDirectory( new File( System.getProperty( "user.dir" ) ) );
+		          int rsp = fc.showSaveDialog(fc) ;
+		          String filename = fc.getSelectedFile().getName();
+		          saveToSVG(filename);
+				}//fin actionPerformed	
+			
+			public void saveToSVG(String xml) {
+				String role1 = null;
+				String role2 = null;
+				String role3 = null;
+				String role4 = null;
+				ArrayList<String> rolev;
+
+			    Document dom;
+			    org.w3c.dom.Element e = null;
+
+			    // instance of a DocumentBuilderFactory
+			    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			    try {
+			        // use factory to get an instance of document builder
+			        DocumentBuilder db = dbf.newDocumentBuilder();
+			        // create instance of DOM
+			        dom = db.newDocument();
+
+			        // create the root element
+			        org.w3c.dom.Element rootEle = dom.createElement("roles");
+
+			        // create data elements and place them under root
+			        e = dom.createElement("role1");
+			        e.appendChild(dom.createTextNode(role1));
+			        rootEle.appendChild(e);
+
+			        e = dom.createElement("role2");
+			        e.appendChild(dom.createTextNode(role2));
+			        rootEle.appendChild(e);
+
+			        e = dom.createElement("role3");
+			        e.appendChild(dom.createTextNode(role3));
+			        rootEle.appendChild(e);
+
+			        e = dom.createElement("role4");
+			        e.appendChild(dom.createTextNode(role4));
+			        rootEle.appendChild(e);
+
+			        dom.appendChild(rootEle);
+
+			        try {
+			            Transformer tr = TransformerFactory.newInstance().newTransformer();
+			            tr.setOutputProperty(OutputKeys.INDENT, "yes");
+			            tr.setOutputProperty(OutputKeys.METHOD, "svg");
+			            tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+			            tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
+			            tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+			            // send DOM to file
+			            tr.transform(new DOMSource(dom), 
+			                                 new StreamResult(new FileOutputStream(xml)));
+
+			        } catch (TransformerException te) {
+			            System.out.println(te.getMessage());
+			        } catch (IOException ioe) {
+			            System.out.println(ioe.getMessage());
+			        }
+			    } catch (ParserConfigurationException pce) {
+			        System.out.println("UsersXML: Error trying to instantiate DocumentBuilder " + pce);
+			    }
+			}//fin methode 
+		}//fin GestEnregistrer
 		
 		
 //fin fenetrePrincipale
