8def55c2c69f22336edd97828c4348d39208681e
unknown <cabredor3@gmail.com>
2019-08-21 12:05:45 -0400

    Ajout javadoc sur certaine classe

--- src/ca/csf/dfc/classes/Formes.java
+++ src/ca/csf/dfc/classes/Formes.java
@@ -4,18 +4,32 @@ import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
-
+/**
+ * Énumération permettant de produire une liste simple de formes
+ * Ceci permet de valider le paramètre des éléments pour la forme de l'objet
+ * 
+ * @author Dominic Legendre
+ *
+ */
 public enum Formes {
 	Rectangle(new Rectangle2D.Double()),
 	Ellipse(new Ellipse2D.Double()),
 	Ligne(new Line2D.Double());
-	
+	/**
+	 * Donnée membre permettant de traduire la String de l'enum en objet Shape utilisable par le programme
+	 */
 	private Shape m_typeElement;
-	
+	/**
+	  * Le paramètre p_typeElement est la forme correspondante en objet Shape
+	  * @param p_typeElement
+	  */
 	Formes(Shape p_typeElement) {
 		this.m_typeElement = p_typeElement;
 	}
-	
+	/**
+     * Retourne un objet de la classe Shape
+     * @return m_typeElement
+     */
 	public Shape getTypeElement() {
 		return this.m_typeElement;
 	}
--- src/ca/csf/dfc/classes/Traits.java
+++ src/ca/csf/dfc/classes/Traits.java
@@ -1,5 +1,11 @@
 package ca.csf.dfc.classes;
-
+/**
+ * Énumération permettant de produire une liste simple de taille de trait
+ * Ceci permet de valider le paramètre des éléments pour la taille des traits
+ * 
+ * @author Dominic Legendre
+ *
+ */
 public enum Traits {
 	UN(1),
 	DEUX(2),
@@ -9,13 +15,22 @@ public enum Traits {
 	DIX(10),
 	DOUZE(12),
 	QUATORZE(14);
-	
+	/**
+	 * Donnée membre permettant de traduire la String de l'enum en integer utilisable par le programme
+	 */
 	private int m_trait;
-	 
+	/**
+	  * Le paramètre p_trait est la taille correspondante en integer
+	  * @param p_couleur
+	  */
     Traits(int p_trait) {
         this.m_trait = p_trait;
     }
- 
+    /**
+     * Retourne un integer représentant la taille trait
+     * 
+     * @return m_trait
+     */
     public int getTrait() {
         return m_trait;
     }
